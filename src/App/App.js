0c7b9fb0d348bec364c92021229dfd57fbe52474
gomaman <999tmll@gmail.com>
2023-06-23 18:02:02 +0300

    REALLY back to start

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import NavigationGenerator from "../Components/Navigation/Navigation";
+import HomePage from "../Pages/Home/HomePage";
+import GamesPage from "../Pages/Games/GamesPage";
+import NewsPage from "../Pages/News/NewsPage";
+import ReviewsPage from "../Pages/Reviews/ReviewsPage";
+import SearchPage from "../Pages/Search/SearchPage";
 
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <NavigationGenerator></NavigationGenerator>
+      <Routes>
+        <Route path='/' element={<HomePage />} ></Route>
+        <Route path='/games' element={<GamesPage />} ></Route>
+
+
+
+
+        <Route path='/news' element={<NewsPage />} ></Route>
+
+
+
+
+
+        <Route path='/reviews' element={<ReviewsPage />} ></Route>
+
+
+
+
+
+        <Route path='/search' element={<SearchPage />} ></Route>
+
+
+
+
+      </Routes>
+
+
+
     </div>
   );
 }
