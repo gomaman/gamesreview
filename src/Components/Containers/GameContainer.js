284cb9a295a879757ac863329ae7824a0e7b0e3f
gomaman <999tmll@gmail.com>
2023-06-24 18:48:43 +0300

    get data from games

--- src/Components/Containers/GameContainer.js
+++ src/Components/Containers/GameContainer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './GameContainer.css';
 import { Button2 } from './Button/Buttons';
 
-const GameContainer = ({ children, buttonText }) => {
+const GameContainer = ({ children, buttonText, titleText }) => {
   return (
     <div className="game-container">
       {children}
       <Button2 className="game-button">{buttonText}</Button2>
+      <p>{titleText}</p>
     </div>
   );
 };
--- src/Pages/Games/GamesPage.js
+++ src/Pages/Games/GamesPage.js
@@ -11,17 +11,19 @@ import { API_URL } from '../../Config/LinksConfig';
 const GamesPage = () => {
     const backgroundImageUrl = 'https://gaming-cdn.com/images/products/268/orig-fallback-v1/the-witcher-3-wild-hunt-pc-game-gog-com-cover.jpg?v=1650526691';
 
+    const [games, setGames] = useState([])
     const [pictures, setPictures] = useState({})
 
     useEffect(() => {
         axios.get(API_URL + '/games?_embed=pictures')
-            .then(res => console.log(res.data))
+            .then(res => {
+                const gamesData = res.data
+                setGames(gamesData)
+            })
     }, [])
 
-
-
-
     return (
+        
         <Container className="games-page-container">
             <section>
                 <PictureContainer
@@ -35,11 +37,15 @@ const GamesPage = () => {
             <section>
                 <div className='games-list-container'>
                     <ul className='games-list'>
-                        <li style={{ backgroundImage: `url(https://gaming-cdn.com/images/products/268/orig-fallback-v1/the-witcher-3-wild-hunt-pc-game-gog-com-cover.jpg?v=1650526691)` }}>
-                            <GameContainer 
-                            buttonText="View Game Details">
-                            </GameContainer>
-                        </li>
+                        {games.map(game => (
+                            <li key={game.id} style={{ backgroundImage: `url(https://gaming-cdn.com/images/products/268/orig-fallback-v1/the-witcher-3-wild-hunt-pc-game-gog-com-cover.jpg?v=1650526691)` }}>
+                                <GameContainer
+                                    key={game.id}
+                                    titleText={game.title}
+                                    buttonText="View Game Details">
+                                </GameContainer>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </section>
