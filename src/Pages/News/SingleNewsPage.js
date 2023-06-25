07c1afbbb3d341f68b4aec3cdf251931633a03af
gomaman <999tmll@gmail.com>
2023-06-25 23:52:18 +0300

    components added

--- src/Pages/News/SingleNewsPage.js
+++ src/Pages/News/SingleNewsPage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Container } from '@mui/material';
+import SingleNewsItem from '../../Components/Containers/SingleNews/SingleNewsComponent';
 
 const SingleNewsPage = () => {
-  return (
-    <div>SingleNewsPage</div>
-  )
-}
 
-export default SingleNewsPage
+  return (
+    <Container>
+      <SingleNewsItem />
+    </Container>
+  );
+};
+
+export default SingleNewsPage;
